517f3fde2f023c2821c3a1e693ff76565e88e7e9
AlbertOkrah <okrahalbert@gmail.com>
2023-02-16 17:17:40 -0800

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19", followed by a newline
+/**
+ * main -and that piece of art is useful
+ * - Dora Korpar, 2015-10-19, followed by a newline
  * Return: Always 1.
  */
 int main(void)
