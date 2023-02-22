b4579b93b3674c714a1f4223319e01aee967c1bb
AlbertOkrah <okrahalbert@gmail.com>
2023-02-21 17:33:48 -0800

    Nested statements, loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,15 @@
  * print_alphabet_x10 - this prints 10 times the alphabet, in lowercase
  */
 void print_alphabet_x10(void)
+
 {
 	char ch;
 	int i;
-	i =0;
-	while (i<10)
+	i = 0;
+	while (i < 10)
 	{
 		ch = 'a';
-		while(ch<='z')
+		while (ch <= 'z')
 		{
 			_putchar(ch);
 			ch++;
